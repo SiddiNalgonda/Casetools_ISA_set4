7b4aa8d5af73a2971e5749daa44e99ca721c3a1b
SiddiNalgonda <srinivasnolgonda@gmail.com>
2024-02-08 10:21:53 +0530

    commit first

--- githubrepository/src/githubrepository/circle.java
+++ githubrepository/src/githubrepository/circle.java
@@ -6,5 +6,18 @@ public class circle {
 	public circle(double radius) {
 		this.radius=radius;
 	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	public void setRadius(double radius) {
+		this.radius=radius;
+	}
+	
+	public double getArea() {
+		return Math.PI*radius*radius;
+	}
 
 }
+
